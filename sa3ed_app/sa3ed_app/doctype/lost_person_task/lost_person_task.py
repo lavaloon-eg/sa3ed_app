612f7d668f697935abe5bd5129a7fe10d6fb2621
abdulrahman-badry777 <abdulrahman.badry777@compit.aun.edu.eg>
2024-09-09 23:12:28 +0300

    Upload the Task LostPerson Doctype & Web view

---
+++ sa3ed_app/sa3ed_app/doctype/lost_person_task/lost_person_task.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, LavaLoon and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
+class LostPersonTask(WebsiteGenerator):
+	pass
