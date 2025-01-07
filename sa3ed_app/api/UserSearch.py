b3b017cd36c4a30eb4c70017f3fe6bb3676b6120
Adham Hebeish <adham.hebeish1@gmail.com>
2025-01-07 13:01:05 +0100

    Fix: Search People Api

--- sa3ed_app/api/UserSearch.py
+++ sa3ed_app/api/UserSearch.py
@@ -11,94 +11,114 @@ def search_people(input):
     key = "%" + input + "%"
 
     # SQL Search Query
-    if input == "":
-        err = "There is no input given"
-        return err
-    query = (
-        frappe.qb.from_(lost_people)
-        .select(
-        lost_people.name,
-        lost_people.creation,
-        lost_people.first_name,
-        lost_people.middle_name,
-        lost_people.last_name,
-        lost_people.nationality,
-        lost_people.gender,
-        lost_people.birthdate.as_("birthdate/age"), #Lost Person has no age (Conflict)
-        lost_people.phone_1,
-        lost_people.phone_2,
-        lost_people.email_address,
-        lost_people.notes,
-        lost_people.case_status,
-        lost_people.lost_date.as_("lost/seen_date"),
-        lost_people.founded_person.as_("founded/lost_person"),
-        lost_people.lost_address.as_("founded/lost_person"),
-        lost_people.home_address.as_("home/hospitality_address")
-        )
-        .where(
-        (lost_people.name.like(key)) |
-        (lost_people.creation.like(key)) |
-        (lost_people.first_name.like(key)) |
-        (lost_people.middle_name.like(key)) |
-        (lost_people.last_name.like(key)) |
-        (lost_people.birthdate.like(key)) |
-        (lost_people.gender.like(key)) |
-        (lost_people.nationality.like(key)) |
-        (lost_people.phone_1.like(key)) |
-        (lost_people.phone_2.like(key)) |
-        (lost_people.email_address.like(key)) |
-        (lost_people.notes.like(key)) |
-        (lost_people.case_status.like(key)) |
-        (lost_people.lost_date.like(key)) |
-        (lost_people.founded_person.like(key)) |
-        (lost_people.home_address.like(key)) |
-        (lost_people.lost_address.like(key))
-        )
-    ) * (
-        frappe.qb.from_(found_people)
-        .select(
-        found_people.name,
-        found_people.creation,
-        found_people.first_name,
-        found_people.middle_name,
-        found_people.last_name,
-        found_people.nationality,
-        found_people.gender,
-        found_people.age, #Found Person has no birthdate (Conflict)
-        found_people.phone_1,
-        found_people.phone_2,
-        found_people.email_address,
-        found_people.notes,
-        found_people.case_status,
-        found_people.seen_date,
-        found_people.lost_person,
-        found_people.seen_address,
-        found_people.hospitality_address
-        )
-        .where(
-        (found_people.name.like(key)) |
-        (found_people.creation.like(key)) |
-        (found_people.first_name.like(key)) |
-        (found_people.middle_name.like(key)) |
-        (found_people.last_name.like(key)) |
-        (found_people.nationality.like(key)) |
-        (found_people.gender.like(key)) |
-        (found_people.age.like(key)) |
-        (found_people.phone_1.like(key)) |
-        (found_people.phone_2.like(key)) |
-        (found_people.email_address.like(key)) |
-        (found_people.notes.like(key)) |
-        (found_people.seen_date.like(key)) |
-        (found_people.case_status.like(key)) |
-        (found_people.lost_person.like(key)) |
-        (found_people.seen_address.like(key)) |
-        (found_people.hospitality_address.like(key))
-        )
-        .orderby(
-            "creation", order=Order.desc
-        )
-    )
-    result = query.run(as_dict=True)
-    
-    #  Return Results
+    try:
+        if input == "":
+            statuscode = 400
+            data = "There is no input given"
+        else:
+            statuscode = 200
+            lost_query = (
+                frappe.qb.from_(lost_people)
+                .select(
+                lost_people.pic,
+                lost_people.first_name,
+                lost_people.middle_name,
+                lost_people.last_name,
+                lost_people.name,
+                lost_people.creation,
+                lost_people.birthdate,
+                lost_people.gender,
+                lost_people.nationality,
+                lost_people.phone_1,
+                lost_people.phone_2,
+                lost_people.email_address,
+                lost_people.notes,
+                lost_people.case_status,
+                lost_people.lost_date,
+                lost_people.founded_person,
+                lost_people.home_address,
+                lost_people.lost_address
+                )
+                .where(
+                (lost_people.pic.like(key)) |
+                (lost_people.name.like(key)) |
+                (lost_people.creation.like(key)) |
+                (lost_people.first_name.like(key)) |
+                (lost_people.middle_name.like(key)) |
+                (lost_people.last_name.like(key)) |
+                (lost_people.birthdate.like(key)) |
+                (lost_people.gender.like(key)) |
+                (lost_people.nationality.like(key)) |
+                (lost_people.phone_1.like(key)) |
+                (lost_people.phone_2.like(key)) |
+                (lost_people.email_address.like(key)) |
+                (lost_people.notes.like(key)) |
+                (lost_people.case_status.like(key)) |
+                (lost_people.lost_date.like(key)) |
+                (lost_people.founded_person.like(key)) |
+                (lost_people.home_address.like(key)) |
+                (lost_people.lost_address.like(key))
+                )
+                .orderby(
+                    "creation", order=Order.desc
+                )
+            )
+            found_query = (
+                frappe.qb.from_(found_people)
+                .select(
+                found_people.pic,
+                found_people.first_name,
+                found_people.middle_name,
+                found_people.last_name,
+                found_people.name,
+                found_people.creation,
+                found_people.nationality,
+                found_people.gender,
+                found_people.age,
+                found_people.phone_1,
+                found_people.phone_2,
+                found_people.email_address,
+                found_people.notes,
+                found_people.seen_date,
+                found_people.case_status,
+                found_people.lost_person,
+                found_people.seen_address,
+                found_people.hospitality_address
+                )
+                .where(
+                (found_people.pic.like(key)) |
+                (found_people.name.like(key)) |
+                (found_people.creation.like(key)) |
+                (found_people.first_name.like(key)) |
+                (found_people.middle_name.like(key)) |
+                (found_people.last_name.like(key)) |
+                (found_people.nationality.like(key)) |
+                (found_people.gender.like(key)) |
+                (found_people.age.like(key)) |
+                (found_people.phone_1.like(key)) |
+                (found_people.phone_2.like(key)) |
+                (found_people.email_address.like(key)) |
+                (found_people.notes.like(key)) |
+                (found_people.seen_date.like(key)) |
+                (found_people.case_status.like(key)) |
+                (found_people.lost_person.like(key)) |
+                (found_people.seen_address.like(key)) |
+                (found_people.hospitality_address.like(key))
+                )
+                .orderby(
+                    "creation", order=Order.desc
+                )
+            )
+            data = {
+                "lost_people" : lost_query.run(as_dict=True),
+                "found_people" : found_query.run(as_dict=True)
+            }
+    except Exception as err:
+        statuscode = 400
+        data = f"An unexpected error happened: {err}"
+
+    result = {
+        "statuscode" : statuscode,
+        "data" : data
+    }
     return result
--- sa3ed_app/www/usersearch/index.js
+++ sa3ed_app/www/usersearch/index.js
@@ -1,5 +1,6 @@
 function UserSearch() {
     document.getElementById("resultdiv").innerHTML = "";
+    document.getElementById("buttondiv").innerHTML = "";
     document.getElementById("err").innerHTML = "";
     var input = document.getElementById("searchbar").value;
     document.getElementById("searchbar").value = "";
@@ -11,40 +12,228 @@ function UserSearch() {
             method: "sa3ed_app.api.UserSearch.search_people",
             args: {input:input},
             callback: function(result) {            
-                var message = result.message;
-                if (message.length == 0) {
-                    document.getElementById("err").className = "message";
-                    document.getElementById("err").innerHTML = "ليس هناك شيء متتابق للبحث";
+                let message = result.message;
+                let data = message.data;
+                if (message.statuscode != 200) {
+                    console.error(data)
+                    document.getElementById("err").className("err");
+                    document.getElementById("err").innerHTML = "هناك خطء في عملية البحث";
                 } else {
-                    for (let index in message) {
-                        var person = message[index];
-                        if (typeof person !== "object") {
+                    let lost_people = data.lost_people;
+                    let found_people = data.found_people;
+                    if (lost_people.length == 0 && found_people.length == 0) {
+                        document.getElementById("err").className = "message";
+                        document.getElementById("err").innerHTML = "ليس هناك شيء متتابق للبحث";
+                    } else {
+                        if (lost_people.length == 0) {
                         } else {
-                            var div = document.createElement("div");
-                            div.className = "person";
-                            div.innerHTML = "<h4>Result " + (Number(index) + 1) + " of " + message.length + "</h4>";
-                            for (const key in person) {
-                                var value = person[key];
-                                if (value == null) {
-                                } else if (value == "") {
+                            let lost_button = document.createElement("button");
+                            lost_button.id = "lost_button";
+                            lost_button.className = "btn btn-primary mx-1";
+                            lost_button.type = "button";
+                            lost_button.onclick = show_lost_people;
+                            lost_button.innerHTML = "Lost People";
+                            document.getElementById("buttondiv").append(lost_button);
+                            document.getElementById("lost_button").focus();
+                            let lost_people_div = document.createElement("div");
+                            lost_people_div.id = "lost_people_div";
+                            let i = 0;
+                            for (let index in lost_people) {
+                                let person = lost_people[index];
+                                if (typeof person !== "object") {
                                 } else {
-                                    var text = document.createElement("p");
-                                    var keytext = key.replaceAll("_"," ");
-                                    let capitalkey = keytext.at(0).toUpperCase();
-                                    keytext = keytext.replace(keytext.at(0), capitalkey);
-                                    text.innerHTML = keytext + ": " + value;
-                                    div.append(text);
+                                    i++
+                                    let div = document.createElement("div");
+                                    div.className = "person";
+                                    div.innerHTML = "<h4>Lost Case " + (Number(index) + 1) + " of " + lost_people.length + "</h4>";
+                                    for (const key in person) {
+                                        if (key == "pic"){
+                                            let img = document.createElement("img");
+                                            let src = person[key];
+                                            let alt;
+                                            if (src == null) {
+                                                src = "/static/img/miss.svg";
+                                                alt = "No Photo Found"
+                                            } else if (src == "") {
+                                                src = "/static/img/miss.svg";
+                                                alt = "No Photo Found"
+                                            } else {
+                                                let srcArray = src.split("/");
+                                                alt = srcArray.pop();
+                                            }
+                                            img.src = src;
+                                            img.alt = alt;
+                                            img.className = "rounded mx-auto d-block";
+                                            img.style.width = "35%";
+                                            div.append(img);
+                                        } else {
+                                            let value = person[key];
+                                            let text = document.createElement("p");
+                                            let keytext = key.replaceAll("_"," ");
+                                            let capitalkey = keytext.at(0).toUpperCase();
+                                            keytext = keytext.replace(keytext.at(0), capitalkey);
+                                            if (value == null) {
+                                                value = "N/A"
+                                            } else if (value == "") {
+                                                value = "N/A"
+                                            }
+                                            if (key == "creation") {
+                                                let dateArray = value.split(" ");
+                                                value = dateArray[0];
+                                            }
+                                            if (key == "name") {
+                                                keytext = "ID";
+                                            }
+                                            text.innerHTML = keytext + ": " + value;
+                                            div.append(text);
+                                        }
+                                    }
+                                    if (i == 4) {
+                                        let show_button = document.createElement("button")
+                                        show_button.id = "show_lost_button";
+                                        show_button.className = "btn btn-primary mx-1";
+                                        show_button.type = "button";
+                                        show_button.onclick = show_more;
+                                        show_button.innerHTML = "Show More";
+                                        lost_people_div.append(show_button)
+                                    } if (i > 3) {
+                                       div.classList.add("showmore");
+                                       div.style.display = "none";
+                                    }
+                                    lost_people_div.append(div);
                                 }
                             }
-                            document.getElementById("resultdiv").append(div);
+                            document.getElementById("resultdiv").append(lost_people_div);
                         }
-                    }  
+                        if (found_people.length == 0) {
+                        } else {
+                            let found_button = document.createElement("button");
+                            found_button.id = "found_button";
+                            found_button.className = "btn btn-primary mx-1";
+                            found_button.type = "button";
+                            found_button.onclick = show_found_people;
+                            found_button.innerHTML = "Found People";
+                            document.getElementById("buttondiv").append(found_button);
+                            let found_people_div = document.createElement("div");
+                            found_people_div.id = "found_people_div";
+                            found_people_div.style.display = "none";
+                            let i = 0;
+                            for (let index in found_people) {
+                                let person = found_people[index];
+                                if (typeof person !== "object") {
+                                } else {
+                                    i++
+                                    let div = document.createElement("div");
+                                    div.className = "person";
+                                    div.innerHTML = "<h4>Found Case " + (Number(index) + 1) + " of " + found_people.length + "</h4>";
+                                    for (const key in person) {
+                                        if (key == "pic"){
+                                            let img = document.createElement("img");
+                                            let src = person[key];
+                                            let alt;
+                                            if (src == null) {
+                                                src = "/static/img/miss.svg";
+                                                alt = "No Photo Found"
+                                            } else if (src == "") {
+                                                src = "/static/img/miss.svg";
+                                                alt = "No Photo Found"
+                                            } else {
+                                                let srcArray = src.split("/");
+                                                alt = srcArray.pop();
+                                            }
+                                            img.src = src;
+                                            img.alt = alt;
+                                            img.className = "rounded mx-auto d-block";
+                                            img.style.width = "35%";
+                                            div.append(img);
+                                        } else {
+                                            let value = person[key];
+                                            let text = document.createElement("p");
+                                            let keytext = key.replaceAll("_"," ");
+                                            let capitalkey = keytext.at(0).toUpperCase();
+                                            keytext = keytext.replace(keytext.at(0), capitalkey);
+                                            if (value == null) {
+                                                value = "N/A"
+                                            } else if (value == "") {
+                                                value = "N/A"
+                                            }
+                                            if (key == "creation") {
+                                                let dateArray = value.split(" ");
+                                                value = dateArray[0];
+                                            }
+                                            if (key == "name") {
+                                                keytext = "ID";
+                                            }
+                                            text.innerHTML = keytext + ": " + value;
+                                            div.append(text);
+                                        }
+                                    }
+                                    if (i == 4) {
+                                        let show_button = document.createElement("button")
+                                        show_button.id = "show_found_button";
+                                        show_button.className = "btn btn-primary mx-1";
+                                        show_button.type = "button";
+                                        show_button.onclick = show_more;
+                                        show_button.innerHTML = "Show More";
+                                        found_people_div.append(show_button)
+                                    } if (i > 3) {
+                                       div.classList.add("showmore");
+                                       div.style.display = "none";
+                                    }
+                                    found_people_div.append(div);
+                                }
+                            }
+                            document.getElementById("resultdiv").append(found_people_div);
+                        }
+                    }
                 }
             },
             error: function(error) {
-                    document.getElementById("err").className("err");
+                document.getElementById("err").className("err");
                 document.getElementById("err").innerHTML = "هناك خطء في عملية البحث";
             }
         });
     }
-} 
+}
+
+function show_lost_people(){
+    if (document.getElementById("resultdiv").innerHTML != "") {
+        document.getElementById("lost_button").focus();
+        document.getElementById("found_button").blur();
+        document.getElementById("lost_people_div").style.display = "block";
+        document.getElementById("found_people_div").style.display = "none";
+        document.getElementById("show_lost_button").style.display = "inline-block";
+        let hiddendivs = document.getElementsByClassName("showmore");
+        for (let index in hiddendivs) {
+            let div = hiddendivs[index]
+            div.style.display = "none";
+        }
+    }
+}
+
+function show_found_people() {
+    if (document.getElementById("resultdiv").innerHTML != "") {
+        document.getElementById("found_button").focus();
+        document.getElementById("lost_button").blur();
+        document.getElementById("found_people_div").style.display = "block";
+        document.getElementById("lost_people_div").style.display = "none";
+        document.getElementById("show_found_button").style.display = "inline-block";
+        let hiddendivs = document.getElementsByClassName("showmore");
+        for (let index in hiddendivs) {
+            let div = hiddendivs[index]
+            div.style.display = "none";
+        }
+    }
+}
+
+function show_more() {
+    if (document.getElementById("resultdiv").innerHTML != "") {
+        document.getElementById("show_found_button").style.display = "none";
+        document.getElementById("show_lost_button").style.display = "none";
+        let hiddendivs = document.getElementsByClassName("showmore");
+        for (let index in hiddendivs) {
+            let div = hiddendivs[index]
+            div.style.display = "block";
+        }
+    }
+}
